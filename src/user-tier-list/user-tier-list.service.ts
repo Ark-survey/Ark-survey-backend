7235caf06341c42c554c8768f7f8dec26701bdc1
GeT_Left <martin_lang@163.com>
2022-05-17 14:23:06 +0800

    move to github;基本统计功能完成

--- src/user-tier-list/user-tier-list.service.ts
+++ src/user-tier-list/user-tier-list.service.ts
@@ -1,5 +1,5 @@
 // user-tier-list.service.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable , Logger} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import { Model } from 'mongoose';
@@ -7,8 +7,15 @@ import { CreateUserTierListDTO, EditUserTierListDTO } from './user-tier-list.dto
 import { UserTierList, UserTierListSimple } from './user-tier-list.interface';
 import { formatUserTierList } from 'utils';
 
+//stat
+import { intervalMapping } from 'utils/stat-utils';
+import { Tier, AllTierListStatistics, Statistic, CharStatistics } from './user-tier-list.interface';
+
+
 @Injectable()
 export class UserTierListService {
+  private readonly logger = new Logger(UserTierListService.name)
+  
   constructor(
     @InjectModel('UserTierList')
     private readonly userTierListModel: Model<UserTierList>,
@@ -56,8 +63,69 @@ export class UserTierListService {
     await this.userTierListModel.findOneAndDelete({ id });
   }
 
-  /** 统计样本总数  */
-  async countAll(): Promise<number> {
-    return await this.userTierListModel.countDocuments({});
+  /** 统计样本总`数  */
+  public async countAll(): Promise<number> {
+    return await this.userTierListModel.countDocuments({ });
+  }
+
+  public async averageAll(): Promise<AllTierListStatistics>{
+    let allTierList: Tier[][] = [];
+    (await this.userTierListModel.find()).map((item) => allTierList.push(item.tierList));
+    
+    let avgTier : CharStatistics = {};
+
+    allTierList.forEach((tierList: Tier[]) => {
+      let values :Array<number> = []; //all values in a tierList。一个人的所有评级
+      tierList.forEach((tier:Tier)=>{
+        values.push(tier.value);
+      });//从tier中提取value
+      // this.logger.debug( "Before mapping ", values);
+      //TODO 只有1个评级怎么办
+      values = intervalMapping(values, 0, 5); //[min, max] 映射到[0,5]
+      // this.logger.debug( "After mapping: ", values);
+      values = values.map(item => 5-item) //reverse，越接近5评分越高
+      // this.logger.debug( "Finally: ", values);
+      tierList.forEach((tier, i) =>{
+        tier.value = values[i];
+      }); //value写回到tier中
+
+      tierList.forEach((tier: Tier)=>{
+        /* tier example:
+        {
+          "value": 0,
+          "characterKeys": [
+            "char_263_skadi"
+          ]
+        }
+        */
+        tier.characterKeys.forEach((optId) => {
+          if((optId in avgTier) == false){
+            avgTier[optId] = {
+              avgValue: 0,
+              count: 0,
+            };
+          };
+          avgTier[optId].count += 1; 
+
+          /*计算平均值：AVG = (N-1)/N * PREV_AVG + cur / N*/
+          const prev_avg : number= avgTier[optId].avgValue;
+          const N : number= avgTier[optId].count;
+          const new_avg = ((N - 1) / N ) * prev_avg + tier.value / N;
+          avgTier[optId].avgValue = new_avg;
+          // this.logger.debug("avgTier[optId].count = " + avgTier.get(optId).count + "avgTier[optId].avgValue = " + avgTier.get(optId).avgValue)
+        });
+      });
+    });
+
+    // for (const [key, value] of Object.entries(avgTier)) {
+    //   this.logger.debug(`干员${key}: 均分${value.avgValue}, 评价次数${value.count}`);
+    // }
+
+    return {
+      count: allTierList.length,
+      charStatistics: avgTier,
+    };
+    
+
   }
 }
